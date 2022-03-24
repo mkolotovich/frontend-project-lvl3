3f0162c984f53461ac7e6b28822a6a0b00032aea
Mike <k2punk@gmail.com>
2022-03-24 09:33:50 +0300

    split addNewPosts function into two functions

--- src/controller.js
+++ src/controller.js
@@ -32,45 +32,83 @@ const validate = (fields) => {
     .then(() => '').catch(() => 'invalidUrl');
 };
 
-const addNewPosts = (state, value, feedId, posts) => {
+const addNewFeed = (state, value, doc, feedId, posts) => {
   const watchedState = state;
+  watchedState.form.feeds.push(value);
+  watchedState.form.processState = 'sent';
+  const { title, description } = doc;
+  const parsedPosts = doc.posts;
+  watchedState.form.feedsDescription.push({ title, description, id: feedId });
+  watchedState.form.posts = parsedPosts;
+  parsedPosts.forEach((el) => posts.push(el));
+  const renderedPosts = document.querySelectorAll('.col-8 .list-group li');
+  renderedPosts.forEach((el) => {
+    const button = el.querySelector('button');
+    button.addEventListener('click', () => {
+      const link = el.querySelector('a');
+      const post = posts.find((item) => item.name === link.textContent);
+      watchedState.form.currentNode = el;
+      post.isReaded = true;
+    });
+  });
+};
+
+const updateFeed = (doc, feedId, posts, state) => {
+  const watchedState = state;
+  const parsedPosts = doc.posts;
+  const newPosts = parsedPosts.map((el) => {
+    const { name } = el;
+    return { name, id: uniqueId(`${feedId}_`) };
+  });
+  const res = [...posts, ...newPosts];
+  const result = uniqBy(res, 'name');
+  watchedState.form.posts = result;
+};
+
+const addNewPosts = (state, value, feedId, posts) => {
+  // const watchedState = state;
   const parsedURL = new URL('https://allorigins.hexlet.app/get');
   parsedURL.searchParams.set('disableCache', 'true');
   parsedURL.searchParams.set('url', value);
   axios.get(parsedURL.href)
     .then((response) => {
       const doc = parse(response.data.contents, feedId);
-      if (!watchedState.form.feeds.includes(value)) {
-        watchedState.form.feeds.push(value);
-        watchedState.form.processState = 'sent';
-        const { title, description } = doc;
-        const parsedPosts = doc.posts;
-        watchedState.form.feedsDescription.push({ title, description, id: feedId });
-        watchedState.form.posts = parsedPosts;
-        parsedPosts.forEach((el) => posts.push(el));
-        const renderedPosts = document.querySelectorAll('.col-8 .list-group li');
-        renderedPosts.forEach((el) => {
-          const button = el.querySelector('button');
-          button.addEventListener('click', () => {
-            const link = el.querySelector('a');
-            const post = posts.find((item) => item.name === link.textContent);
-            watchedState.form.currentNode = el;
-            post.isReaded = true;
-          });
-        });
+      // if (!watchedState.form.feeds.includes(value)) {
+      if (!state.form.feeds.includes(value)) {
+        // watchedState.form.feeds.push(value);
+        // watchedState.form.processState = 'sent';
+        // const { title, description } = doc;
+        // const parsedPosts = doc.posts;
+        // watchedState.form.feedsDescription.push({ title, description, id: feedId });
+        // watchedState.form.posts = parsedPosts;
+        // parsedPosts.forEach((el) => posts.push(el));
+        // const renderedPosts = document.querySelectorAll('.col-8 .list-group li');
+        // renderedPosts.forEach((el) => {
+        //   const button = el.querySelector('button');
+        //   button.addEventListener('click', () => {
+        //     const link = el.querySelector('a');
+        //     const post = posts.find((item) => item.name === link.textContent);
+        //     watchedState.form.currentNode = el;
+        //     post.isReaded = true;
+        //   });
+        // });
+        addNewFeed(state, value, doc, feedId, posts);
       } else {
-        const parsedPosts = doc.posts;
-        const newPosts = parsedPosts.map((el) => {
-          const { name } = el;
-          return { name, id: uniqueId(`${feedId}_`) };
-        });
-        const res = [...posts, ...newPosts];
-        const result = uniqBy(res, 'name');
-        watchedState.form.posts = result;
+        // const parsedPosts = doc.posts;
+        // const newPosts = parsedPosts.map((el) => {
+        //   const { name } = el;
+        //   return { name, id: uniqueId(`${feedId}_`) };
+        // });
+        // const res = [...posts, ...newPosts];
+        // const result = uniqBy(res, 'name');
+        // watchedState.form.posts = result;
+        updateFeed(doc, feedId, posts, state);
       }
-      setTimeout(addNewPosts, 5000, watchedState, value, feedId, watchedState.form.posts);
+      // setTimeout(addNewPosts, 5000, watchedState, value, feedId, watchedState.form.posts);
+      setTimeout(addNewPosts, 5000, state, value, feedId, state.form.posts);
     })
     .catch((e) => {
+      const watchedState = state;
       watchedState.form.processError = e.message;
       watchedState.form.processState = 'error';
     });
